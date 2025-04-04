02bef514a1d353fb3334ec411b2d79494b1b46f7
Charleskojomark <markcharleskojo@gmail.com>
2025-04-04 16:16:12 +0100

    perf(core) add database indexes for optimized queries

--- app/core/database.py
+++ app/core/database.py
@@ -5,5 +5,12 @@ from pymongo.errors import ConnectionFailure
 load_dotenv()
 
 client = AsyncIOMotorClient(os.getenv("MONGO_URI"))
-db = client["url_shortener"]
+db = client.get_database()
+urls_collection = db.urls 
 
+def create_indexes():
+    urls_collection.create_index([("short_url", 1)], unique=True)
+    urls_collection.create_index([("original_url", 1)], unique=True)
+    urls_collection.create_index([("short_hash", 1)], unique=True)
+    
+create_indexes()
