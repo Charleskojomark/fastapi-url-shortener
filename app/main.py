9dd0ec649c6246f4fdbe4db5d8eaa4a96d532894
Charleskojomark <markcharleskojo@gmail.com>
2025-04-04 16:52:40 +0100

    refactor(main) add db indexes on startup

--- app/main.py
+++ app/main.py
@@ -1,9 +1,16 @@
 from fastapi import FastAPI
 import uvicorn 
 from routes.url import router as url_router
+from core.database import init_db
+from contextlib import asynccontextmanager
 
-app = FastAPI(title="URL Shortener API", version="1.0.0")
 
+@asynccontextmanager
+async def lifespan(app: FastAPI):
+    await init_db()
+    yield
+
+app = FastAPI(title="URL Shortener API", version="1.0.0", lifespan=lifespan)
 app.include_router(url_router)
 
 if __name__ == "__main__":
